1490c36c67b450db835817a2a50fef2caefb8176
srimathi412 <srimathi4125@gmail.com>
2026-01-22 23:39:11 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+from xgboost import XGBRegressor
